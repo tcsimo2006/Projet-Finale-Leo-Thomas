3f5a9fe022f861ff579e0aade0efb07cb6c748c4
Leo <leo.saracino.q@gmail.com>
2024-05-28 18:04:10 +0000

    May 28

--- cartscript.js
+++ cartscript.js
@@ -2,3 +2,24 @@ const logo =document.querySelector("img")
 logo.addEventListener("click", function() {
   window.location.href = "index.html"
 })
+
+function CartLoad(){
+  alert(Cart.length)
+  Cart.forEach(e => {
+      if (e.quantity !=0)
+          {
+            alert()
+              const CarDiv = document.createElement("div");
+                      CarDiv.setAttribute("class", "Cardiv")
+                      CarDiv.innerHTML = `
+                      <img src="${e.image}" class="CartImg" alt="...">
+                      <h4 class="cart-h3">${e.nom} ${e.prix}$</h4>
+                      <button class="CartButton" id="CartPlus" onclick ="PlusQuantity(${e.index})">+</button>
+                      <h4 class="cart-h3" id>${e.quantity}</h4>
+                      <button class="CartButton" id="CartMinus" onclick = "moinsQuantity(${e.index})">-</button>
+                      `
+            document.getElementById("cart").appendChild(CarDiv)
+          }
+  })
+}
+CartLoad()
--- script.js
+++ script.js
@@ -1,3 +1,132 @@
+let BreadItems = [
+    {
+        index: 0,
+        quantity: 0,
+        nom: "Sourdough",
+        image: src = "Resrouces/IMG/Sourdough.gif",
+        prix: 25
+    },
+    {
+        index: 1,
+        quantity: 0,
+        nom: 'Bagels',
+        image: src = "Resrouces/IMG/Bagels.jpg",
+        prix: 15.99
+    },
+    {
+        index: 2,
+        quantity: 0,
+        nom: 'Baguette',
+        image: src = "Resrouces/IMG/Bagueetes.jpeg",
+        prix: 13.99
+    },
+    {
+        index: 3,
+        quantity: 0,
+        nom: 'Challah',
+        image: src = "Resrouces/IMG/Challah.jpg",
+        prix: 32.99
+    },
+]
+let CakeItems =[
+  {
+    index: 0,
+    quantity: 0,
+    nom: "Smith Island Cake",
+    image: src = "Resrouces/IMG/smith.jpg",
+    prix: 75
+  },
+  {
+    index: 1,
+    quantity: 0,
+    nom: 'Funfetti Cake',
+    image: src = "Resrouces/IMG/funfetti_cake_49993_16x9.jpg",
+    prix: 125
+  },
+  {
+    index: 2,
+    quantity: 0,
+    nom: 'Red Velvet Cake',
+    image: src = "Resrouces/IMG/Red Velvet.jpeg",
+    prix: 200
+  },
+  {
+    index: 3,
+    quantity: 0,
+    nom: 'Flan',
+    image: src = "Resrouces/IMG/Flan.jpg",
+    prix: 45
+  },
+]
+let SandItems =[
+  {
+    index: 0,
+    quantity: 0,
+    nom: "Ham And Cheese Sandwhich",
+    image: src = "Resrouces/IMG/HC.jpg",
+    prix: 12
+  },
+  {
+    index: 1,
+    quantity: 0,
+    nom: 'Steak Panini',
+    image: src = "Resrouces/IMG/SS.jpg",
+    prix: 15
+  },
+  {
+    index: 2,
+    quantity: 0,
+    nom: 'Caprese Panino',
+    image: src = "Resrouces/IMG/CS.jpg",
+    prix: 10
+  },
+  {
+    index: 3,
+    quantity: 0,
+    nom: 'Croque Monsieur',
+    image: src = "Resrouces/IMG/CM.jpg",
+    prix: 9
+  },
+  {
+    index: 4,
+    quantity: 0,
+    nom: 'Chicken Club',
+    image: src = "Resrouces/IMG/ClubS.jpg",
+    prix: 20
+  },
+]
+let PastItems =[
+  {
+    index: 0,
+    quantity: 0,
+    nom: "Croissant",
+    image: src = "Resrouces/IMG/Cross.jpg",
+    prix: 4
+  },
+  {
+    index: 1,
+    quantity: 0,
+    nom: 'New York Roll',
+    image: src = "Resrouces/IMG/NYC.jpg",
+    prix: 14
+  },
+  {
+    index: 2,
+    quantity: 0,
+    nom: 'Sugar Cookie',
+    image: src = "Resrouces/IMG/SC.jpg",
+    prix: 1.99
+  },
+  {
+    index: 3,
+    quantity: 0,
+    nom: 'Lemon Bars',
+    image: src = "Resrouces/IMG/LB.jpg",
+    prix: 2.99
+  },
+]
+let items = [BreadItems, CakeItems, PastItems, SandItems]
+var Cart = []	
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -34,4 +163,50 @@ function openNav(name){
 function closeNav(name){
   document.getElementById(name).style.width = "0"
   document.getElementById(name).style.zIndex = 0
+}
+function AddCart(Group, ind)
+{ 
+  alert()
+  let j=0;
+  if (Cart.length ==0) {
+    Cart.push(items[Group, ind])
+    items[Group, ind].quantity ++
+    const CarDiv = document.createElement("div");
+    CarDiv.setAttribute("class", "Cardiv")
+    CarDiv.innerHTML = `
+    <img src="${items[Group, ind].image}" class="CartImg" alt="...">
+    <h4 class="cart-h3">${items[Group, ind].nom} ${items[Group, ind].prix}$</h4>
+    <button class="CartButton" id="CartPlus" onclick="PlusQuantity(${items[Group, ind].index})">+</button>
+    <h4 class="cart-h3">${items[Group, ind].quantity}</h4>
+    <button class="CartButton" id="CartMinus" onclick="moinsQuantity(${items[Group, ind].index})">-</button>
+    `
+    document.getElementById("cart").appendChild(CarDiv)
+  }
+  Cart.forEach(item => {
+    if (item.index == index) {
+        if(Cart.length > 0)
+            {
+                PlusQuantity(item.index)
+                return
+            }
+        else {
+          j++
+          if (j == Cart.length) {
+          Cart.push(items[Group, ind])
+          items[Group, ind].quantity ++
+          const CarDiv = document.createElement("div");
+          CarDiv.setAttribute("class", "Cardiv")
+          CarDiv.innerHTML = `
+          <img src="${items[Group, ind].image}" class="CartImg" alt="...">
+          <h4 class="cart-h3">${items[Group, ind].nom} ${items[Group, ind].prix}$</h4>
+          <button class="CartButton" id="CartPlus" onclick="PlusQuantity(${items[Group, ind].index})">+</button>
+          <h4 class="cart-h3">${items[Group, ind].quantity}</h4>
+          <button class="CartButton" id="CartMinus" onclick="moinsQuantity(${items[Group, ind].index})">-</button>
+          `
+          document.getElementById("cart").appendChild(CarDiv)
+          }
+        }  
+      }
+  });
+  items[Group,ind].quantity = 0;
 }
